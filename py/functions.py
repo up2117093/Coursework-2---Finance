684f8dcf80654784006519fbd1e5f88221510878
Paris <up2117093@myport.ac.uk>
2026-07-21 13:21:15 +0100

    added functions.py, still needs improvements

---
+++ py/functions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def basic_clean(df):
+    df = df.dropna()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date')
+    return df
+
+def train_test_split_time(df, test_size=0.2):
+    split = int(len(df) * (1 - test_size))
+    train = df.iloc[:split]
+    test = df.iloc[split:]
+    return train, test
+=
+
